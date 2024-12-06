9ac8e343332b46834b16aff842c41fcda5ec1351
ravi-harley <ravikharley@outlook.com>
2024-12-06 11:14:28 -0500

    made SIRS
    added model with immunity waning after 2 months

--- R/SIRS_Model.R
+++ R/SIRS_Model.R
@@ -41,6 +41,37 @@ sir_out <- lsoda(init,times,sir_ode,parms)
 sir_out_long <- melt(as.data.frame(sir_out),"time")
 
 #Plotting the model output
-ggplot(sir_out_long,aes(x=time,y=value,colour=variable,group=variable))+
+SIR <-ggplot(sir_out_long,aes(x=time,y=value,colour=variable,group=variable))+
     geom_line(lwd=2)+             #Add line
     xlab("Time")+ylab("Number")   #Add labels
+
+print(SIR)
+
+parms2 <- c(alpha= 2.123,        # alpha = daily contacts
+           beta=0.581,        # beta = probability of infection on contact
+           sigma=0.609,       # sigma = rate of recovery per day
+           mu = 0.0,        # mu =  per capita birth and death rate
+           omega = 1/60)     # omega = rate of immune loss per day
+
+
+sirs_ode <- function(times,init,parms2){
+    with(as.list(c(parms2,init)), {
+        # ODEs
+        dS <- mu*(S+I+R) + omega*R -alpha*beta*I*S/(S+I+R) - mu*S 
+        dI <- alpha*beta*I*S/(S+I+R)-sigma*I - mu*I
+        dR <- sigma*I  - omega*R - mu*R
+        list(c(dS,dI,dR))
+    })
+}
+
+# This creates the output from model equations.  
+#If you want to run the model for longer, change the second term eg: seq(0,200,...)
+times <- seq(0,100,length.out=100)
+sirs_out <- lsoda(init,times,sirs_ode,parms2)
+sirs_out_long <- melt(as.data.frame(sirs_out),"time")
+
+SIRS <-ggplot(sirs_out_long,aes(x=time,y=value,colour=variable,group=variable))+
+    geom_line(lwd=2)+             #Add line
+    xlab("Time")+ylab("Number") #Add labels
+
+print(SIRS)
