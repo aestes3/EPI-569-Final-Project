7176d428afebaa9e72c06e3ed84fb22a5e6cebe1
ravi-harley <ravikharley@outlook.com>
2024-12-06 09:49:38 -0500

    SIRS_model
    Added code from Aiden

---
+++ R/SIRS_Model.R
@@ -0,0 +1,10 @@
+# Load Library
+library(here)
+library(tidyverse)
+library(kableExtra)
+library(deSolve)
+library(reshape2)
+
+#Load data
+rff <- readRDS(here("/Users/ravikineu/Desktop/EPI 569/FINAL/EPI-569-Final-Project/data/rff_2024.rds"))
+
